42564f0514bbb0ae6c5031a0c22ea40f9f8b6bec
MIKTHATGUY <74175183+MIKTHATGUY@users.noreply.github.com>
2022-07-06 15:00:35 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,6 +1,8 @@
 import os 
-import json
+import json, datetime
 n = 0
+n2 = 0
+oggi = datetime.datetime.now().strftime("%d/%m/%Y")
 for item in os.listdir("./Bambini/"):
     with open(f"./Bambini/{item}/{item}.json", "r") as jsonFile:
         data = json.load(jsonFile)
@@ -8,5 +10,15 @@ for item in os.listdir("./Bambini/"):
     if data["mensag"] == "Si":
         n = n+1
 
-print("Numero di bambini che mangiano: ", n)
 
+
+
+
+for item in os.listdir("./Bambini/"):
+    with open(f"./Bambini/{item}/{item}.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
+    if data["giorni"][-1] == oggi:
+        n2 = n+1
+print("Numero di bambini presenti: ", n2)
+print("Numero di bambini che mangiano: ", n)
